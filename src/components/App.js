a6664e5f5ab06b8bbd575d97172827736fd577fd
memematvaz <memematvaz@gmail.com>
2020-06-11 20:11:08 +0200

    header

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import '../styles/App.scss';
 import '../index.scss';
 
+
+import './Header.js';
+
+
 import Header from './Header';
 
 import { Route, Switch } from 'react-router-dom';
 
-class App extends React.Component {
-  
-}
+const App = () => (
+  <div className="App">
+    <Switch>
+      <Route exact path="/">
+        <Header/>
+      </Route>
+    
+
+
+    <Route path="/about" />
+  </Switch>
+</div>
+)
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-  
+const Header = () => {
+
+
+  return (
+   <header class="header">
+    <Link to="/">
+      <h1>María Mateo</h1>
+    </Link>
+    <Link to="About.js">
+      <div>About</div>
+    </Link>
+       </header>
+
+
+
+
+  )
+}
+
+export default Header;
