1ccc2243d87ea595b2e398a8b03f203655819daf
memematvaz <memematvaz@gmail.com>
2020-06-12 14:57:01 +0200

    main

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,21 +8,21 @@ const Main = () => {
   return (
    <div className="main">
         <Link className="link" to="/codeProjects">
-            <div className="code-projects">
+            <div className="code projects">
                 <img src="#" alt="code project" className="main-image-code-projects"></img>
-                <h2>Code</h2>
+                <h2>Code projects</h2>
             </div>
         </Link>
         <Link className="link" to="/photoProjects">
-            <div className="photo-projects">
+            <div className="photo projects">
                 <img src="#" alt="photo project" className="main-image-photo-projects"></img>
-                <h2>Photo</h2>
+                <h2>Photo projects</h2>
             </div>
         </Link>
         <Link className="link" to="/videoProjects">
-            <div className="video-projects">
+            <div className="video projects">
                 <img src="#" alt="video project" className="main-image-video-projects"></img>
-                <h2>Video</h2>
+                <h2>Video projects</h2>
             </div>
         </Link>
 
