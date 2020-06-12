ab2c545ebcf3f92ce5235481720f7dd5a94e1889
memematvaz <memematvaz@gmail.com>
2020-06-12 21:04:57 +0200

    main photos

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import code from '../images/code-red.png';
+import photo from '../images/photo-red.png';
+
 
 const Main = () => {
 
@@ -9,19 +12,19 @@ const Main = () => {
    <div className="main">
         <Link className="link" to="/codeProjects">
             <div className="code projects">
-                <img src="#" alt="code project" className="main-image-code-projects"></img>
+                <img src={code} alt="code project" className="main-image-projects"></img>
                 <h2>Code projects</h2>
             </div>
         </Link>
         <Link className="link" to="/photoProjects">
             <div className="photo projects">
-                <img src="#" alt="photo project" className="main-image-photo-projects"></img>
+                <img src={photo} alt="photo project" className="main-image-projects"></img>
                 <h2>Photo projects</h2>
             </div>
         </Link>
         <Link className="link" to="/videoProjects">
             <div className="video projects">
-                <img src="#" alt="video project" className="main-image-video-projects"></img>
+                <img src="#" alt="video project" className="main-image-projects"></img>
                 <h2>Video projects</h2>
             </div>
         </Link>
