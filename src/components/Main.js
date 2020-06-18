c53d4ac9f454133b54a89a9f68b54bbef7e8957f
memematvaz <memematvaz@gmail.com>
2020-06-18 19:19:13 +0200

    code-project-animation

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,19 +13,19 @@ const Main = () => {
    <div className="main">
         <Link className="link" to="/codeProjects">
             <div className="code projects">
-                <img src={code} alt="code project" title="code project"  className="main-image-projects"></img>
+                <img src={code} alt="personal computer" title="personal computer"  className="main--image-projects"></img>
                 <h2>Code projects</h2>
             </div>
         </Link>
         <Link className="link" to="/photoProjects">
             <div className="photo projects">
-                <img src={photo} alt="photo project" title="photo project" className="main-image-projects"></img>
+                <img src={photo} alt="camera" title="camera" className="main--image-projects"></img>
                 <h2>Photo projects</h2>
             </div>
         </Link>
         <Link className="link" to="/videoProjects">
             <div className="video projects">
-                <img src={video} alt="video project" title="video project"  className="main-image-projects"></img>
+                <img src={video} alt="film" title="film"  className="main--image-projects"></img>
                 <h2>Video projects</h2>
             </div>
         </Link>
--- src/components/projects/details/CodeProjects.js
+++ src/components/projects/details/CodeProjects.js
@@ -4,16 +4,51 @@ import FilterButtons from './FilterButtons';
 
 
 const CodeProjects = () => {
- const [projects, setFilteredProjects] = useState(0)
+ const [projects, setFilteredProjects] = useState()
 
     return (
         <>
           <Header/>
           <FilterButtons/>
-          <div className="project-list">
+          <div className="code-project--list">
               {projects} 
+
+
+              <div className="container">
+            <div className="div" >
+            <div className="text">I see you!</div>
+              <img className="img" src="https://www.interactius.com/wp-content/uploads/2017/09/foto6.png" title="Foto6" alt="Foto6"></img>
+
+      
           </div>
+          </div>
+
+
+          <div className="container">
+            <div className="div" >
+            <div className="text">I see you!</div>
+              <img className="img" src="https://www.interactius.com/wp-content/uploads/2017/09/foto6.png" title="Foto6" alt="Foto6"></img>
+
+      
+          </div>
+          </div>
+
+
+
+
+
+          </div>
+
+
+
+
+         
+
         </>
+
+
+
+
   
   
   
