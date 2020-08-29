c07dc63052c9a096d59b77f2e41496c618f906e4
memematvaz <memematvaz@gmail.com>
2020-08-30 01:57:24 +0200

    better text

--- src/components/services/CodeProjectsJSON.js
+++ src/components/services/CodeProjectsJSON.js
@@ -6,7 +6,7 @@ const CODEPROJECTS =[
         title: 'Jara Amaro psicologist',
         details: {
                 tools:'React - SASS',
-                description:'Design and development of the professional website of psychologist Jara Amaro. The website describes her work and has a contact form. Made using React, SASS and Formspree.',
+                description:'I designed and developed the professional website of psychologist Jara Amaro. The website describes her work, gives information about her fees and allows you to contact her using a form. It was developed using React, SASS and Formspree.',
                 links: {
                     code:'https://github.com/memematvaz/jara-amaro-psychologist',
                     web:'https://memematvaz.github.io/jara-amaro-psychologist/',
@@ -47,7 +47,7 @@ const CODEPROJECTS =[
         title: 'Rick&Morty Character filter',
         details: {
                 tools:'React - SASS - API - LocalStorage',
-                description:'Catalogue of the characters of the Rick&Morty TV series using an API.',
+                description:'Catalogue of the characters of the Rick&Morty TV serie using an API. You can find, using the searcher, the different characters and get more information about them. Made using React and SASS. This is the final exam of the Adalab bootcamp.',
                 links: {
                     code:'https://github.com/memematvaz/Rick-Morty-character-filter-react',
                     web:'http://beta.adalab.es/modulo-3-evaluacion-final-memematvaz/',
@@ -61,7 +61,7 @@ const CODEPROJECTS =[
         title: 'Crypto currencies',
         details: {
                 tools:'JS - Bootstrap - API',
-                description:'This application uses an API to develop a cryptomoney quoter.',
+                description:"This application uses an API to develop a cryptomoney quoter. Developed in JavaScript vanilla using Bootstrap and applying object oriented programming. It was done in Udemy's course Modern JavaScript. ",
                 links: {
                     code:'https://github.com/memematvaz/Cryptocurrency-API',
                     web:'https://memematvaz.github.io/Cryptocurrency-API/',
@@ -75,7 +75,7 @@ const CODEPROJECTS =[
         title: 'Translator Mimimi',
         details: {
                 tools:'React',
-                description:'This application can be defined as Mimimi translator. Everything you write will be automatically translated into mimimi.',
+                description:"This application can be defined as Mimimi translator. It is freely inspired by the Los Punsetes's song called Opinión de mierda which can be heard on it. Everything you write will be automatically translated into mimimi.",
                 links: {
                     code:'https://github.com/memematvaz/translator-mimimi',
                     web:'https://memematvaz.github.io/translator-mimimi/',
@@ -89,7 +89,7 @@ const CODEPROJECTS =[
         title: 'Ecommerce photoshots',
         details: {
                 tools:['Js', 'Bootstrap', 'LocalStorage' ],
-                description:'Example of an ecommerce website where you can add different photo sessions to the shopping cart and remove them from it.',
+                description:'Example of an e-commerce website where you can add different photo sessions to the shopping cart and remove them from it. It was made using bootstrap and it uses localStorage to save the cart information. It was my first side project.',
                 links: {
                     code:'https://github.com/memematvaz/shopping-cart-photoshoots-js',
                     web:'https://memematvaz.github.io/shopping-cart-photoshoots-js/',
@@ -103,7 +103,7 @@ const CODEPROJECTS =[
         title: 'Sheckies',
         details: {
                 tools:'SASS',
-                description:'Corporate website fully developed with HTML5 and CSS using SASS.',
+                description:'This website is the first group project of the Adalab bootcamp. It is a corporate website made using SASS that has a functional contact form thanks to Formspree. We follow a design given by Zeplin and we describe our own group as if it were a company.',
                 links: {
                     code:'https://github.com/memematvaz/Sheckies',
                     web:'http://beta.adalab.es/project-promo-i-module-1-team-4-afternoon/',
@@ -117,7 +117,7 @@ const CODEPROJECTS =[
         title: 'Guess the number',
         details: {
                 tools:'JS',
-                description:'This is a small game made with JavaScript in which a number is automatically generated and the user must guess it using the clues.',
+                description:'This is a small game made with JavaScript in which a number is automatically generated and the user must guess it using the clues. This is an exam of the JavaScript module made in the Adalab bootcamp.',
                 links: {
                     code:'https://github.com/memematvaz/Guess-the-number',
                     web:'https://memematvaz.github.io/Guess-the-number/',
